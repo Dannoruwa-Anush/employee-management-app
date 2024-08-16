7a5ef8377348a9781e051c33a8a1ce40f800dd47
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2024-08-16 16:52:17 +0530

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,2 +1,6 @@
-//front-end URL
-export const BASE_URL = 'http://192.168.8.124:3000';
+//connect to backend
+export const URL = 'http://192.168.8.124';  //backend_URL
+export const PORT = 3000; //backend_PORT
+
+export const BASE_URL = `${URL}:${PORT}`;
+
