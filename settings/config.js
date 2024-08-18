f661f3419f2b93d181687109a0da3d505ef8af31
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2024-08-18 08:32:14 +0530

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,5 +1,5 @@
 //connect to backend
-export const URL = 'http://192.168.8.124';  //backend_URL
+export const URL = 'http://10.32.11.27';  //backend_URL (Metro waiting on exp://10.32.11.27)
 export const PORT = 3000; //backend_PORT
 
 export const BASE_URL = `${URL}:${PORT}`;
